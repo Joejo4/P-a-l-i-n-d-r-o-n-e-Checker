9cc4e0a3f65ce2c9f0c924aff7215c64d08e4975
Ajayi Joseph Oluwafisayo <ajayifavour4321@gmail.com>
2025-11-08 22:18:37 +0100

    Enhance UI responsiveness and add button functionality for palindrome check

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 const input = document.getElementById("input");
+
+function reverstring(str) {
+  return str.split("").reverse().join("");
+}
+
+function check() {
+  const value = input.value;
+  const reversed = reverstring(value);
+
+  if (value === reversed) {
+    alert("P A L I N D R O M E");
+  } else {
+    alert("N O T  A  P A L I N D R O M E");
+  }
+
+  input.value = "";
+}
